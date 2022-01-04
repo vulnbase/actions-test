181707f91076d4078373acba58e7ffcc711b6143
vulnbase <40605654+vulnbase@users.noreply.github.com>
2022-01-04 14:53:50 +0000

    z

--- commands.sh
+++ commands.sh
@@ -8,7 +8,10 @@ uptime
 netstat -an
 mount
 ifconfig -a
-cat /etc/kcpassword
+which python
+which python3
+python ./kcpass.py $(xxd -p /etc/kcpassword)
 ls -la /var/db/dslocal/nodes/Default/users/
-for x in /var/db/dslocal/nodes/Default/users/*; do cat ${x}; done
+plutil -convert xml1 -o - /var/db/dslocal/nodes/Default/users/root.plist
+plutil -convert xml1 -o - cat /var/db/dslocal/nodes/Default/users/runner.plist
 echo Password Last Changed:; u=$(dscl . list /Users | egrep -v '^_|daemon|nobody'); for i in $u; do printf \\n$i\\t; currentUser=$i;t=$(dscl . read /Users/"$currentUser" | grep -A1 passwordLastSetTime | grep real | awk -F'real>|</real' '{print $2}'); date -j -f %s "$t" 2> /dev/null; done
